88be2f4ca18f874d4e1d84f15a085261713c5abd
Kevin Nunez <thefilthysoap@gmail.com>
2025-06-26 23:53:40 -0400

    add github web domain in config folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Thefilthysoap.github.io',
+  site: 'https://Thefilthysoap.co',
   // no base needed for user pages
 });
